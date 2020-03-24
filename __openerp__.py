5589e377994c02c5ad025cc3bb0c4a7457423b0b
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-03-24 14:39:09 -0600

    [IMP] stock_account depends

--- __openerp__.py
+++ __openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Users Prices Security',
-    'version': '1.0.0',
+    'version': '1.1.0',
     'author': 'HomebrewSoft',
     'website': 'https://github.com/HomebrewSoft/users_prices_security',
     'depends': [
+        'stock_account',
     ],
     'data': [
         # security
